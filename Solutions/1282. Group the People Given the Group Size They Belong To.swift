6b304dc7e023c64b0d5a147ddb0100650a1bdc7c
Loyi Hsu <loyi.hsu@gmail.com>
2020-04-15 13:46:55 +0800

    Create 1342, 1282.

---
+++ Solutions/1282. Group the People Given the Group Size They Belong To.swift
@@ -0,0 +1,52 @@
+// Problem: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+
+class Solution {
+    struct group {
+        var members: [Int]
+        var count: Int
+        var limit: Int
+    }
+    
+    func groupThePeople(_ groupSizes: [Int]) -> [[Int]] {
+        var i = 0
+        var groups = [group]()
+        var found = false
+        
+        var output = [[Int]]()
+
+        var constructed = [Int]()
+        
+        var groupTemplate = group.init(members: [Int](), count: 0, limit: 0)
+        
+        for g in groupSizes {
+            if constructed.contains(g) == false {
+                groupTemplate.limit = g
+                groups.append(groupTemplate)
+                constructed.append(g)
+            }
+        }
+        
+        while i < groupSizes.count {
+            for g in 0..<groups.count where groups[g].limit == groupSizes[i] && groups[g].count < groups[g].limit {
+                found = true
+                groups[g].members.append(i)
+                groups[g].count += 1
+
+                if groups[g].count == groups[g].limit {
+                    output.append(groups[g].members)
+                    groups[g].members.removeAll()
+                    groups[g].count = 0
+                }
+
+                break
+            }
+            i += 1
+        }
+        
+        for g in 0..<groups.count where groups[g].count != 0 {
+            output.append(groups[g].members)
+        }
+        
+        return output
+    }
+}
---
+++ Solutions/1342. Number of Steps to Reduce a Number to Zero.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+class Solution {
+    func numberOfSteps (_ num: Int) -> Int {
+        var n = num
+        var steps = 0
+        
+        while n != 0 {
+            if n % 2 == 0 {
+                n /= 2
+            } else {
+                n -= 1
+            }  
+            
+            steps += 1
+        }
+        
+        return steps
+    }
+}
