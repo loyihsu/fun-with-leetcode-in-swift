83b90f89abecc6f6f3f2e9e938007f7963db92e5
Loyi Hsu <loyi.hsu@gmail.com>
2020-04-23 00:34:06 +0800

    Create 1221.

---
+++ Solutions/1221. Split a String in Balanced Strings.swift
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+class Solution {
+    func balancedStringSplit(_ s: String) -> Int {
+        var lefts = 0
+        var rights = 0
+        var o = [String]()
+        var temp = ""
+        
+        for c in s {
+            if c == "R" { rights += 1 }
+            else if c == "L" { lefts += 1 }
+            
+            temp.append(c)
+            
+            if lefts == rights && temp.isEmpty == false {
+                o.append(temp)
+                temp = ""
+            }
+        }
+        
+        return o.count
+    }
+}
