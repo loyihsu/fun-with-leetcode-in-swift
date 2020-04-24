665fbb6512f2c12e1746065c0fb7bd2bf9798e14
loyihsu <loyi.hsu@gmail.com>
2020-04-24 11:02:19 +0800

    Create 0283.

---
+++ Solutions/0283. Move Zeroes.swift
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var temp = 0
+        for i in 0..<nums.count where nums[i] == 0 {
+            for j in i..<nums.count where nums[j] != 0 {
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                break
+            }
+        }
+    }
+}
