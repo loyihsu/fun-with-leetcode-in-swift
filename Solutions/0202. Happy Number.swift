726497664015d97cc76c178aee3a7ec332d48841
Loyi Hsu <loyi.hsu@gmail.com>
2020-05-06 11:56:42 +0800

    Create 0202.

---
+++ Solutions/0202. Happy Number.swift
@@ -0,0 +1,32 @@
+// Problem: https://leetcode.com/problems/happy-number/
+
+class Solution {
+    func getSqrDigs(_ number: Int) -> Int {
+        var n = number
+        var output = 0
+        var a = -1
+        
+        while n > 0 {
+            a = n % 10
+            output += a * a
+            n /= 10
+        }
+        
+        return output
+    }
+    
+    func isHappy(_ n: Int) -> Bool {
+        var temp = getSqrDigs(n)        
+        var dic = [Int : Bool]()
+        
+        while temp != 1 {
+            if dic[temp] == true {
+                return false
+            }
+            dic[temp] = true
+            temp = getSqrDigs(temp)
+        }
+        
+        return true
+    }
+}
