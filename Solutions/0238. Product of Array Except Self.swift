47bf37239adaadd482a3db2f2f44866a3e79d8b7
loyihsu <loyi.hsu@gmail.com>
2020-05-11 21:23:03 +0800

    Create 0238.

---
+++ Solutions/0238. Product of Array Except Self.swift
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var right = [Int](), output = [Int](), temp = 1
+        let ncount = nums.count
+        
+        for index in 0..<ncount {
+            right.append(temp)
+            temp *= nums[ncount-index-1]
+        }
+        
+        temp = 1
+        
+        for index in 0..<ncount {
+            output.append(temp * right[ncount-index-1])
+            temp *= nums[index]
+        }
+        
+        return output
+    }
+}
