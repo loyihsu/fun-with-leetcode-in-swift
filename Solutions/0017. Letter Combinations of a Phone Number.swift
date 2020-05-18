10f05d432876d6f55b0be44bb04478d0a7249294
loyihsu <loyi.hsu@gmail.com>
2020-05-18 16:16:52 +0800

    Create 0017.

---
+++ Solutions/0017. Letter Combinations of a Phone Number.swift
@@ -0,0 +1,33 @@
+// Problem: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution {
+    func letterCombinations(_ digits: String) -> [String] {
+        let searchMap = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        var output = [String]()
+        
+        for digit in digits {
+            var temp = [String]()
+            
+            for t in output {
+                if let d = digit.wholeNumberValue {
+                    for c in searchMap[d] {
+                        temp.append("\(t)\(c)")
+                    }
+                }
+                
+            }
+            
+            if temp.isEmpty {
+                if let d = digit.wholeNumberValue {
+                    for c in searchMap[d] {
+                        temp.append("\(c)")
+                    }
+                }
+            }
+            
+            output = temp
+        }
+        
+        return output
+    }
+}
