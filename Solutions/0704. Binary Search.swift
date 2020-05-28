a14483efb1e7a484112ba7f2c6ef7d646564285a
loyihsu <loyi.hsu@gmail.com>
2020-05-28 16:14:46 +0800

    Create 0704.

---
+++ Solutions/0704. Binary Search.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/binary-search/
+
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var lower = 0, upper = nums.count, pos = -1
+        
+        while lower < upper {
+            pos = (upper - lower) / 2 + lower
+            if target == nums[pos] {
+                return pos
+            } else if target > nums[pos] {
+                lower = pos + 1
+            } else {
+                upper = pos
+            }
+        }
+        
+        return -1
+    }
+}
