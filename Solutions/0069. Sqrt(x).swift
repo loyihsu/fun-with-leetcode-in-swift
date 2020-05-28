26f49ac9c85031ef183fb8d9fe8a65e3dec8a85b
loyihsu <loyi.hsu@gmail.com>
2020-05-29 05:02:06 +0800

    Create 0069.

---
+++ Solutions/0069. Sqrt(x).swift
@@ -0,0 +1,29 @@
+// Problem: https://leetcode.com/problems/sqrtx/
+
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        guard x >= 2 else { return x }
+        
+        var output = -1
+        
+        var left = 0,  right = x
+        var mid = -1, cal = -1
+        
+        while left <= right {
+            mid = (left + right) / 2
+            cal = mid * mid
+            
+            if cal == x {
+                output = mid
+                break
+            } else if cal > x {
+                right = mid - 1
+            } else if cal < x {
+                output = mid
+                left = mid + 1
+            }
+        }
+        
+        return output
+    }
+}
