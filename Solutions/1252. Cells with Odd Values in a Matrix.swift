9d3bd9f54de75ad1f915c67e985199c740d053a3
loyihsu <loyi.hsu@gmail.com>
2020-06-09 15:15:41 +0800

    Create 1252.

---
+++ Solutions/1252. Cells with Odd Values in a Matrix.swift
@@ -0,0 +1,31 @@
+// Problem: https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+
+class Solution {
+    func oddCells(_ n: Int, _ m: Int, _ indices: [[Int]]) -> Int {
+        var matrix = [[Bool]](repeating: [Bool](repeating: false, count: m),
+                             count: n)
+        for rc in indices {
+            let r = rc[0]
+            let c = rc[1]
+            for i in 0..<m {
+                matrix[r][i] = !matrix[r][i]
+            }
+            for i in 0..<n {
+                matrix[i][c] = !matrix[i][c]
+            }
+        }
+        
+        var odd = 0
+        
+        for arr in matrix {
+            for a in arr {
+                if a {
+                    odd += 1
+                }
+            }
+        }
+        
+        return odd
+        
+    }
+}
