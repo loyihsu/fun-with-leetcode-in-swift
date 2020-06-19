3bb915ce3b9d7771f3cfa850e34b0879a535f00f
loyihsu <loyi.hsu@gmail.com>
2020-06-19 15:18:27 +0800

    Create 1476.

---
+++ Solutions/1476. Subrectangle Queries.swift
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/subrectangle-queries/
+
+class SubrectangleQueries {    
+    init(_ rectangle: [[Int]]) {
+        self.rectangleObject = rectangle
+    }
+    
+    func updateSubrectangle(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int, _ newValue: Int) {
+        for i in row1...row2 {
+            for j in col1...col2 {
+                rectangleObject[i][j] = newValue
+            }
+        }
+    }
+    
+    func getValue(_ row: Int, _ col: Int) -> Int {
+        return rectangleObject[row][col]
+    }
+    
+    var rectangleObject: [[Int]]
+}
