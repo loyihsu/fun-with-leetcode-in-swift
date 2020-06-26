3dc835f51405643cf7b20ed74c73d86eadc93b14
Loyi Hsu <loyi.hsu@gmail.com>
2020-06-26 18:00:43 +0800

    Create 0804.

---
+++ Solutions/0804. Unique Morse Code Words.swift
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/unique-morse-code-words/
+
+class Solution {
+    func uniqueMorseRepresentations(_ words: [String]) -> Int {
+        var output = Set<String>()
+        
+        let characters = Array("abcdefghijklmnopqrstuvwxyz"), morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        var map = [Character: String](), temp = ""
+        
+        for i in 0..<26 {
+            map[characters[i]] = morse[i]
+        }
+        
+        for word in words {
+            temp = ""
+            for c in word {
+                temp += map[c]!
+            }
+            output.insert(temp)
+        }
+        
+        return output.count
+    }
+}
+
