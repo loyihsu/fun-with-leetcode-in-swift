0c903ba053a0bd17976019fbf45c4ff3bd553128
loyihsu <loyi.hsu@gmail.com>
2020-07-10 22:27:05 +0800

    Create 1299.

---
+++ Solutions/1299. Replace Elements with Greatest Element on Right Side.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+class Solution {
+    func replaceElements(_ arr: [Int]) -> [Int] {
+        var temp = -1, large = -1, array = arr, index = arr.count - 1
+        
+        while index >= 0 {
+            if array[index] > temp {
+                temp = array[index]
+            }
+            
+            array[index] = large
+            large = temp
+            index -= 1
+        }
+        
+        return array
+    }
+}
