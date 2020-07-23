2704f3d5afe425f5a2d9d052cc0a19b42f991383
loyihsu <loyi.hsu@gmail.com>
2020-07-23 17:40:35 +0800

    Create 0160.

---
+++ Solutions/0160. Intersection of Two Linked Lists.swift
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+class Solution {
+    func createStack(from head: ListNode?) -> [ListNode] {
+        var res = [ListNode](), temp = head
+        while let t = temp {
+            res.append(t)
+            temp = t.next
+        }
+        return res
+    }
+    
+    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        var a = createStack(from: headA), b = createStack(from: headB), res: ListNode? = nil
+        while a.isEmpty == false && b.isEmpty == false {
+            var i = a.removeLast(), j = b.removeLast()
+            if i === j {
+                res = i 
+            } else {
+                break
+            }
+        }
+        return res
+    }
+}
