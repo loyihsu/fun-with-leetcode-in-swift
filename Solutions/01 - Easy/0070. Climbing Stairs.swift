da89a20500f84bb0709c8b1f336057496bb696bd
loyihsu <loyi.hsu@gmail.com>
2020-08-03 14:00:48 +0800

    Create 0070, update 0075.

---
+++ Solutions/01 - Easy/0070. Climbing Stairs.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        guard n != 1 else { return 1 }
+        guard n != 2 else { return 2 }
+        var gen = (1,2), times = n - 2
+        while times > 0 {
+            gen = (gen.1, gen.0+gen.1)
+            times -= 1
+        }
+        return gen.1
+    }
+}
--- Solutions/02 - Medium/0075. Sort Colors.swift
+++ Solutions/02 - Medium/0075. Sort Colors.swift
@@ -1,29 +1,26 @@
 // Problem: https://leetcode.com/problems/sort-colors/
 
 class Solution {
-    func swapAt(_ nums: inout [Int], _ a: Int, _ b: Int) {
-        var temp = nums[a]
-        nums[a] = nums[b]
-        nums[b] = temp
-    }
-    
     func qsort(_ nums: inout [Int], _ min: Int, _ max: Int) {
+        guard min < max && min >= 0 && max < nums.count else { return }
         var i = min, j = max
         let standard = nums[min]
-
-        if min > max { return }
 
         while i != j {
             while nums[j] >= standard && i < j { j -= 1 }
             while nums[i] <= standard && i < j { i += 1 }
-            if i < j { swapAt(&nums, i, j) }
+            if i < j {
+                var temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
         }
 
         nums[min] = nums[i]
         nums[i] = standard
 
-        if i - 1 > 0 { qsort(&nums, min, i-1) }
-        if i + 1 < nums.count { qsort(&nums, i+1, max) }
+        qsort(&nums, min, i-1)
+        qsort(&nums, i+1, max)
     }
     
     func sortColors(_ nums: inout [Int]) {
