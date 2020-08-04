da223aa2ff36cac73a0e155aaf78985f74b19412
loyihsu <loyi.hsu@gmail.com>
2020-08-04 16:21:03 +0800

    Create 0083.

---
+++ Solutions/01 - Easy/0083. Remove Duplicates from Sorted List.swift
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class Solution {
+    func createList(_ arr: [Int]) -> ListNode? {
+        var list: ListNode? = nil, index = arr.count-1
+        while index >= 0 {
+            list = ListNode.init(arr[index], list)
+            index -= 1
+        }
+        return list
+    }
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var last = Int.max, h = head, res = [Int]()
+        while let temp = h {
+            if temp.val != last { res.append(temp.val) }
+            last = temp.val
+            h = temp.next
+        }
+        return createList(res)
+    }
+}
