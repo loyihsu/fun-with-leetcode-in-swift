0dd16d353d324f85552dee6aa62200b7179f1d79
loyihsu <loyi.hsu@gmail.com>
2020-08-07 19:22:25 +0800

    Create 0961.

---
+++ Solutions/01 - Easy/0961. N-Repeated Element in Size 2N Array.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+class Solution {
+    func repeatedNTimes(_ A: [Int]) -> Int {
+        let repeatedTimes = A.count / 2
+        var map = [Int:Int]()
+        
+        for item in A {
+            map[item] = (map[item] ?? 0) + 1
+        }
+        
+        for (key, value) in map where value == repeatedTimes {
+            return key
+        }
+        
+        return -1
+    }
+}
