aa1eb3ef0004f34f44d1d16e394bd00b7497460e
loyihsu <loyi.hsu@gmail.com>
2020-08-10 13:00:32 +0800

    Create 1534.

---
+++ Solutions/01 - Easy/1534. Count Good Triplets.swift
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/count-good-triplets/
+
+class Solution {
+    func countGoodTriplets(_ arr: [Int], _ a: Int, _ b: Int, _ c: Int) -> Int {
+        var count = 0
+        for k in 0..<arr.count {
+            for j in 0..<k where abs(arr[j]-arr[k]) <= b {
+                for i in 0..<j where abs(arr[i]-arr[j]) <= a && abs(arr[i]-arr[k]) <= c {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+}
