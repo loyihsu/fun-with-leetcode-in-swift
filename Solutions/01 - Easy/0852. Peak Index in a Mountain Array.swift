224044398f891937d6298f1b9e40128607f3f0cf
loyihsu <loyi.hsu@gmail.com>
2020-08-19 12:59:46 +0800

    Create 0852.

---
+++ Solutions/01 - Easy/0852. Peak Index in a Mountain Array.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution {
+    func peakIndexInMountainArray(_ A: [Int]) -> Int {
+        var maxIndex = -1, max = -1
+        
+        for (pos, val) in A.enumerated() {
+            if val > max {
+                maxIndex = pos
+                max = val
+            } else if val < max {
+                break
+            }
+        }
+        
+        return maxIndex
+    }
+}
