66e6b616d82a09825e97601ffc82de48dfdef000
loyihsu <loyi.hsu@gmail.com>
2020-09-01 23:24:07 +0800

    Create 1403.

---
+++ Solutions/01 - Easy/1403. Minimum Subsequence in Non-Increasing Order.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/
+
+class Solution {
+    func minSubsequence(_ nums: [Int]) -> [Int] {
+        let sortedNums = nums.sorted(by: >)
+        var pickedSum = 0, notPickedSum = sortedNums.reduce(0, +), pos = 0
+        
+        while pickedSum <= notPickedSum {
+            pickedSum += sortedNums[pos]
+            notPickedSum -= sortedNums[pos]
+            pos += 1
+        }
+        
+        return Array(sortedNums[0..<pos])
+    }
+}
