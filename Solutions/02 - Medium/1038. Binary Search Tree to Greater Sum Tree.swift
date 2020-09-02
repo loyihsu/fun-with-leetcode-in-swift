07947bf3cf33d5743a176976bdcdac198502aa46
loyihsu <loyi.hsu@gmail.com>
2020-09-03 02:45:01 +0800

    Create 1038.

---
+++ Solutions/02 - Medium/1038. Binary Search Tree to Greater Sum Tree.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+class Solution {
+    func bstToGst(_ root: TreeNode?) -> TreeNode? {
+        var sum = 0
+        search(root, sum: &sum)
+        
+        return root
+    }
+    func search(_ root: TreeNode?, sum: inout Int) {
+        guard let root = root else { return }
+        
+        search(root.right, sum: &sum)
+        sum += root.val
+        root.val = sum
+        search(root.left, sum: &sum)
+    }
+}
