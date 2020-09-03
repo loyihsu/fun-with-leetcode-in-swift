85814f68a6c0f22ac3a8597bd06a70dc8454e88f
loyihsu <loyi.hsu@gmail.com>
2020-09-03 23:31:58 +0800

    Update 1561.

--- Solutions/02 - Medium/1561. Maximum Number of Coins You Can Get.swift
+++ Solutions/02 - Medium/1561. Maximum Number of Coins You Can Get.swift
@@ -1,8 +1,20 @@
 // Problem: https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
 
 class Solution {
+    func mySort(_ piles: [Int]) -> [Int] {
+        var temp = [Int](repeating: 0, count: 10001), output = piles, pos = 0
+        piles.forEach { temp[$0] += 1 }
+        for (index, value) in temp.enumerated() where value > 0 {
+            for _ in 0..<value {
+                output[pos] = index
+                pos += 1
+            }
+        }
+        return output
+    }
+    
     func maxCoins(_ piles: [Int]) -> Int {
-        var sortedPile = piles.sorted(), sum = 0, pos = 0
+        var sortedPile = mySort(piles), sum = 0, pos = 0
         
         while piles.count - pos * 2 - 2 > pos {
             sum += sortedPile[piles.count - pos * 2 - 2]           
