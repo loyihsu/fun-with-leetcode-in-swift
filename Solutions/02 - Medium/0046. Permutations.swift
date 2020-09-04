06cb51e4288b906994b8ce62e9db74f4f9b27adf
loyihsu <loyi.hsu@gmail.com>
2020-09-04 13:53:46 +0800

    Create 0046.

---
+++ Solutions/02 - Medium/0046. Permutations.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/permutations/
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        guard nums.count > 1 else { return [nums] }
+        guard nums.count > 2 else { return [nums, nums.reversed() ]}
+        var output = [[Int]]()
+        for (index, value) in nums.enumerated() {
+            var temp = nums
+            temp.remove(at: index)
+            permute(temp).forEach {
+                var cur = [value]
+                cur.append(contentsOf: $0)
+                output.append(cur)
+            }
+        }
+        return output
+    }
+}
