82e4cdf54d25174104dccdfe5c15f4e92530ee5e
loyihsu <loyi.hsu@gmail.com>
2020-09-09 22:22:20 +0800

    Create 1304.

---
+++ Solutions/01 - Easy/1304. Find N Unique Integers Sum up to Zero.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+
+class Solution {
+    func sumZero(_ n: Int) -> [Int] {
+        guard n > 1 else { return [0] }
+        var output = [Int](), resting = n
+        
+        if resting % 2 == 1 {
+            output.append(0)
+            resting -= 1
+        }
+        for val in 1...resting/2 {
+            output.append(val)
+            output.append(-val)
+        }
+        
+        return output
+    }
+}
