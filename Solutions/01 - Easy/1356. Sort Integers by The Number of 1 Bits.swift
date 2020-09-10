cba8681c1bfc7dc0fa479b9f9a76cac7ed9e7c6e
loyihsu <loyi.hsu@gmail.com>
2020-09-10 13:18:21 +0800

    Create 1356.

---
+++ Solutions/01 - Easy/1356. Sort Integers by The Number of 1 Bits.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+
+class Solution {
+    func countOnes(_ num: Int) -> Int {
+        var num = num, count = 0
+        while num > 0 {
+            count += num % 2
+            num /= 2
+        }
+        return count
+    }
+    func generateBitMap(_ arr: [Int]) -> [Int : [Int]] {
+        var output = [Int : [Int]]()
+        arr.forEach { output[countOnes($0), default: []].append($0) }
+        return output
+    }
+    func sortByBits(_ arr: [Int]) -> [Int] {
+        return generateBitMap(arr).sorted { $0.key < $1.key }.flatMap { $1.sorted() }
+    }
+}
