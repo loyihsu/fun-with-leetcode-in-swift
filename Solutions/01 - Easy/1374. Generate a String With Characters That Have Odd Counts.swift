298f141c53f24a896f908742f4ae24da3ec1ec8f
loyihsu <loyi.hsu@gmail.com>
2020-09-14 13:19:19 +0800

    Create 1374.

---
+++ Solutions/01 - Easy/1374. Generate a String With Characters That Have Odd Counts.swift
@@ -0,0 +1,11 @@
+// Problem: https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+
+class Solution {
+    func generateTheString(_ n: Int) -> String {
+        var output = Array<Character>(repeating: "a", count: n)
+        if output.count % 2 == 0 {
+            output[output.count-1] = "b"
+        }
+        return String(output)
+    }
+}
