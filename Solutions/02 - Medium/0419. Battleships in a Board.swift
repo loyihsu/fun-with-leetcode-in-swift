161af47323a10db50422a5e39ef39a87ef6d2540
loyihsu <loyi.hsu@gmail.com>
2020-09-15 12:47:13 +0800

    Create 0419.

---
+++ Solutions/02 - Medium/0419. Battleships in a Board.swift
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/battleships-in-a-board/
+
+class Solution {
+    func countBattleships(_ board: [[Character]]) -> Int {
+        var count = 0
+        for index_i in board.indices {
+            for index_j in board[index_i].indices where board[index_i][index_j] == "X" {
+                if (index_i < 1 || board[index_i-1][index_j] != "X") && (index_j < 1 || board[index_i][index_j-1] != "X") {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+}
