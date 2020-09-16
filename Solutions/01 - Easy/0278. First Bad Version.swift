ecc2b6dab9e2c26411e72b337111259fa4021485
loyihsu <loyi.hsu@gmail.com>
2020-09-17 04:23:02 +0800

    Create 0278.

---
+++ Solutions/01 - Easy/0278. First Bad Version.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/first-bad-version/
+
+class Solution : VersionControl {
+    func firstBadVersion(_ n: Int) -> Int {
+        var left = 0, right = n, temp = n
+        while left <= right {
+            let mid = (left + right) / 2
+            if isBadVersion(mid) == true {
+                temp = mid
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return temp
+    }
+}
