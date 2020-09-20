ed0130364c5a78a1df43f35a011d9cda93381f54
loyihsu <loyi.hsu@gmail.com>
2020-09-21 04:04:51 +0800

    Create 1588.

---
+++ Solutions/01 - Easy/1588. Sum of All Odd Length Subarrays.swift
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+
+class Solution {
+    func sumOddLengthSubarrays(_ arr: [Int]) -> Int {
+        var sum = arr.reduce(0, +), size = 3
+        while size <= arr.count {
+            if arr.count == size {
+                sum += arr.reduce(0, +)
+            } else {
+                for index in 0...arr.count-size {
+                    var temp = 0
+                    var debug = [Int]()
+                    for offset in 0..<size {
+                        temp += arr[index+offset]
+                        debug.append(arr[index+offset])
+                    }
+                    sum += temp
+                }
+            }
+            size += 2
+        }
+        return sum
+    }
+}
