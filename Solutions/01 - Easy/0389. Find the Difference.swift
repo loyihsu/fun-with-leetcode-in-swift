11831d54fe0a619b7b295f460d85873e846e81d1
loyihsu <loyi.hsu@gmail.com>
2020-09-30 01:34:49 +0800

    Create 0389.

---
+++ Solutions/01 - Easy/0389. Find the Difference.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/find-the-difference/
+
+class Solution {
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        var map = [Character : Int](), result = Character("z")
+        s.forEach { map[$0] = (map[$0] ?? 0) + 1 }
+        for char in t {
+            if let temp = map[char], temp > 0 {
+                map[char] = temp - 1
+            } else {
+                result = char
+            }
+        }
+        return result
+    }
+}
+
