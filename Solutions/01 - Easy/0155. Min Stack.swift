b9f1317a81411d78b5487c1b57757015535bf9fc
loyihsu <loyi.hsu@gmail.com>
2020-10-01 20:15:40 +0800

    Create 0155.

---
+++ Solutions/01 - Easy/0155. Min Stack.swift
@@ -0,0 +1,33 @@
+// Problem: https://leetcode.com/problems/min-stack/
+
+class MinStack {
+    var min = Int.max
+    var array = [Int]()
+    
+    /** initialize your data structure here. */
+    init() {
+        
+    }
+    
+    func push(_ x: Int) {
+        array.append(x)
+        min = x < min ? x : min
+    }
+    
+    func pop() {
+        let last = array.popLast()!
+        if min == last {
+            min = Int.max
+            array.forEach { min = $0 < min ? $0 : min }
+        }
+    }
+    
+    func top() -> Int {
+        return array.last!
+    }
+    
+    func getMin() -> Int {
+        return min
+    }
+}
+
