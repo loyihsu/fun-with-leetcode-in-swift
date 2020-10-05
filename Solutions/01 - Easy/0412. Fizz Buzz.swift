edb81cc4417132d985aaa072de4cc081342fe57f
loyihsu <loyi.hsu@gmail.com>
2020-10-05 18:38:35 +0800

    Create 0412.

---
+++ Solutions/01 - Easy/0412. Fizz Buzz.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/fizz-buzz/
+
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var output = [String](repeating: "", count: n)
+        for number in 1...n {
+            if number % 3 == 0 && number % 5 == 0 {
+                output[number-1] = "FizzBuzz"
+            } else if number % 3 == 0 {
+                output[number-1] = "Fizz"
+            } else if number % 5 == 0 {
+                output[number-1] = "Buzz"
+            } else {
+                output[number-1] = "\(number)"
+            }
+        }
+        return output
+    }
+}
