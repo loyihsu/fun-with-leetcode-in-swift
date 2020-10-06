9961734bcf655ccadbb1adf6298ce244093b872c
loyihsu <loyi.hsu@gmail.com>
2020-10-06 14:21:33 +0800

    Create 0108.

---
+++ Solutions/01 - Easy/0108. Convert Sorted Array to Binary Search Tree.swift
@@ -0,0 +1,11 @@
+// Problem: https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+class Solution {
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        guard nums.count > 0 else { return nil }
+        let center = nums.count / 2, node = TreeNode(nums[center])
+        node.left = sortedArrayToBST(Array(nums[0..<center]))
+        node.right = sortedArrayToBST(Array(nums[center+1..<nums.count]))
+        return node
+    }
+}
