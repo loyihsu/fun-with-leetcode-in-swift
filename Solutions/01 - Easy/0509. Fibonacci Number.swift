a8955fcf443858e10c01fab81f67ab2eb592ea3c
loyihsu <loyi.hsu@gmail.com>
2020-10-09 02:21:47 +0800

    Create 0509.

---
+++ Solutions/01 - Easy/0509. Fibonacci Number.swift
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+    func fib(_ N: Int) -> Int {
+        guard N != 0 else { return 0 }
+        guard N != 1 else { return 1 }
+        var num = [1, 0]
+        for i in 0..<N-1 {
+            var temp = num[0] + num[1]
+            num[1] = num[0]
+            num[0] = temp
+        }
+        return num[0]
+    }
+}
