3e35ca680c0ffe9b8d322fcd8828c7565557e80e
loyihsu <loyi.hsu@gmail.com>
2020-10-13 16:17:12 +0800

    Create 0404 & 0599.

---
+++ Solutions/01 - Easy/0404. Sum of Left Leaves.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/sum-of-left-leaves/
+
+class Solution {
+    func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
+        guard root != nil else { return 0 }
+        var list: [(TreeNode,String)] = [(root!, "root")], output = 0
+        while let temp = list.popLast() {
+            if temp.0.left == nil && temp.0.right == nil && temp.1 == "left" {
+                output += temp.0.val
+            } else {
+                if let node = temp.0.left { list.append((node, "left")) }
+                if let node = temp.0.right { list.append((node, "right")) }
+            }
+        }
+        return output
+    }
+}
---
+++ Solutions/01 - Easy/0599. Minimum Index Sum of Two Lists.swift
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+
+class Solution {
+    func minimum(_ map: [String:Int]) -> Int {
+        var min = Int.max
+        for (_, value) in map where value < min {
+            min = value
+        }
+        return min
+    }
+    func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
+        var map = [String:Int](), commons = list1.filter { list2.contains($0) }
+        for (index, value) in list1.enumerated() where commons.contains(value) {
+            map[value] = index
+        }
+        for (index, value) in list2.enumerated() where commons.contains(value) {
+            map[value] = (map[value] ?? 0) + index
+        }
+        let min = minimum(map)
+        return Array(map.filter { $0.value == min }.keys)
+    }
+}
