7363ee326efd7bac7e46ace61bbeb93f054c5743
loyihsu <loyi.hsu@gmail.com>
2020-10-21 16:42:08 +0800

    Create 1619.

---
+++ Solutions/01 - Easy/1619. Mean of Array After Removing Some Elements.swift
@@ -0,0 +1,8 @@
+// Problem: https://leetcode.com/problems/mean-of-array-after-removing-some-elements/
+
+class Solution {
+    func trimMean(_ arr: [Int]) -> Double {
+        let chunkedSortedArray = arr.sorted()[Int(Double(arr.count) * 0.05)..<arr.count-Int(Double(arr.count) * 0.05)]
+        return Double(chunkedSortedArray.reduce(0,+))/Double(chunkedSortedArray.count)
+    }
+}
