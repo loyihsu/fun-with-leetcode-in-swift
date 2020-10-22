4a941043fd0eaf089d79736160f5fa2a50dde56b
loyihsu <loyi.hsu@gmail.com>
2020-10-22 13:33:40 +0800

    Create 1556.

---
+++ Solutions/01 - Easy/1556. Thousand Separator.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/thousand-separator/
+
+class Solution {
+    func thousandSeparator(_ n: Int) -> String {
+        var string = Array("\(n)"), index = string.count, count = -1
+        while index >= 0 {
+            if count == 2 && index != 0 {
+                string.insert(".", at: index)
+                count = 0
+            } else {
+                count += 1 
+            }
+            index -= 1
+        }
+        return String(string)
+    }
+}
