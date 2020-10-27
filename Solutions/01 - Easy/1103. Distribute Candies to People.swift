709cf59d2a251daab5068264064aed228a606e70
loyihsu <loyi.hsu@gmail.com>
2020-10-27 20:25:52 +0800

    Create 1103.

---
+++ Solutions/01 - Easy/1103. Distribute Candies to People.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/distribute-candies-to-people/
+
+class Solution {
+    func distributeCandies(_ candies: Int, _ num_people: Int) -> [Int] {
+        var restingCandy = candies, cur = 0, output = [Int](repeating: 0, count: num_people)
+        while restingCandy > 0 {
+            cur += 1
+            output[(cur-1)%num_people] += restingCandy >= cur ? cur : restingCandy
+            restingCandy -= restingCandy >= cur ? cur : restingCandy
+        }
+        return output
+    }
+}
