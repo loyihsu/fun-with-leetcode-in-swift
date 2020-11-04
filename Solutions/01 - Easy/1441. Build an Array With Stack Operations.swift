ab649b5e5d6168fad0bbfd00fae42526d92cb921
loyihsu <loyi.hsu@gmail.com>
2020-11-05 02:19:51 +0800

    Create 1441.

---
+++ Solutions/01 - Easy/1441. Build an Array With Stack Operations.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/build-an-array-with-stack-operations/
+
+class Solution {
+    func buildArray(_ target: [Int], _ n: Int) -> [String] {
+        var tar = target , output = [String]()
+        for number in 1...n where tar.isEmpty == false {
+            output.append("Push")
+            if tar.first == number {
+                tar.remove(at: 0)
+            } else {
+                output.append("Pop")
+            }
+        }
+        return output
+    }
+}
