14a4f7a8aabdae080fd029d2fe4fa22f21f74fc7
loyihsu <loyi.hsu@gmail.com>
2020-11-06 01:42:22 +0800

    Create 1185.

---
+++ Solutions/01 - Easy/1185. Day of the Week.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/day-of-the-week/
+
+class Solution {    
+    func leap(_ year: Int) -> Bool {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 
+    }
+    func dayOfTheWeek(_ day: Int, _ month: Int, _ year: Int) -> String {
+        var sumDays = 0
+        let days = leap(year) ? [0,31,60,91,121,152,182,213,244,274,305,335] : [0,31,59,90,120,151,181,212,243,273,304,334]
+        let results = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+        for cur in 1971..<year {
+            sumDays += leap(cur) ? 366 : 365
+        }
+        sumDays += days[month-1] + day - 1
+        return results[(sumDays) % 7]
+    }
+}
