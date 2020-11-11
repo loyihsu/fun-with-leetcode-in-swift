08e1bb4f5dc71707bb9003ea8fa9100a1afb4abf
loyihsu <loyi.hsu@gmail.com>
2020-11-12 02:54:43 +0800

    Create 0374.

---
+++ Solutions/01 - Easy/0374. Guess Number Higher or Lower.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/guess-number-higher-or-lower/
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {    
+        var lower = 1, higher = n
+        while true {
+            var mid = (lower + higher)/2
+            if guess(mid) == 0 {
+                return mid
+            } else if guess(mid) == 1 {
+                lower = mid + 1
+            } else if guess(mid) == -1 {
+                higher = mid - 1
+            }
+        }
+    }
+}
