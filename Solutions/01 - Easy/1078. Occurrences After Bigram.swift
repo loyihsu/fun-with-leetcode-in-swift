dca8d5c27c9739bbbfd4ec4963b2b21646f49327
loyihsu <loyi.hsu@gmail.com>
2020-11-16 03:40:11 +0800

    Create 1078.

---
+++ Solutions/01 - Easy/1078. Occurrences After Bigram.swift
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/occurrences-after-bigram/
+
+class Solution {
+    func findOcurrences(_ text: String, _ first: String, _ second: String) -> [String] {
+        var components = text.components(separatedBy: .whitespaces), index = 0, output = [String]()
+        for item in components {
+            if (index == 0 || index == 1) && item == first {
+                index = 1
+            } else if index == 1 && item == second {
+                index += 1
+            } else if index == 2 {
+                if item == first {
+                    index = 1
+                } else {
+                    index = 0
+                }
+                output.append(item)
+            } else {
+                index = 0
+            }
+        }
+        return output
+    }
+}
