aa155032cee41a7a2318e701474f8599af953ed7
loyihsu <loyi.hsu@gmail.com>
2020-11-24 14:41:52 +0800

    Create 0151.

---
+++ Solutions/02 - Medium/0151. Reverse Words in a String.swift
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/reverse-words-in-a-string/
+
+class Solution {
+    func reverseWords(_ s: String) -> String {
+        var temp = "", stack = [String]()
+        for char in s {
+            if char == " " && temp.isEmpty == false {
+                stack.append(temp)
+                temp = ""
+            } else if char != " " {
+                temp.append(char)
+            }
+        }
+        if temp.isEmpty == false {
+            stack.append(temp)
+        }
+        temp = ""
+        while let element = stack.popLast() {
+            temp.append(element)
+            temp.append(" ")
+        }
+        temp.popLast()
+        return temp
+    }
+}
