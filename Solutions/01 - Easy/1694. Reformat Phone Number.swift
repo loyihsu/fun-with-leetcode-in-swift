28f347a6c646c2cc0753926f0ebfbc6f2f8d88fe
loyihsu <loyi.hsu@gmail.com>
2020-12-21 13:50:34 +0800

    Create 1694.

---
+++ Solutions/01 - Easy/1694. Reformat Phone Number.swift
@@ -0,0 +1,33 @@
+// Problem: https://leetcode.com/problems/reformat-phone-number/
+
+class Solution {
+    func reformatNumber(_ number: String) -> String {
+        var temp = String(number.filter { !" -".contains($0) }.reversed()), output = ""
+        while temp.count >= 2 {
+            if output.isEmpty == false {
+                output.append("-")
+            }
+            if temp.count > 4 {
+                for _ in 0..<3 {
+                    output.append(temp.popLast()!)
+                }
+            } else if temp.count == 4 {
+                for idx in 0..<4 {
+                    if idx == 2 {
+                        output.append("-")
+                    }
+                    output.append(temp.popLast()!)
+                }
+            } else if temp.count == 3 {
+                for _ in 0..<3 {
+                    output.append(temp.popLast()!)
+                }
+            } else {
+                for _ in 0..<2 {
+                    output.append(temp.popLast()!)
+                }
+            }
+        }
+        return output
+    }
+}
