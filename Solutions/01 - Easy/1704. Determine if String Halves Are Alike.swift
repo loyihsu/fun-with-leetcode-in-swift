7e70fd34b06e25b2cebc04f50128dc98f5bf2f4c
loyihsu <loyi.hsu@gmail.com>
2020-12-28 23:59:28 +0800

    Create 1704.

---
+++ Solutions/01 - Easy/1704. Determine if String Halves Are Alike.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/determine-if-string-halves-are-alike/
+
+class Solution {
+    private func countVowels(in string: Substring) -> Int {
+        return string.filter { "aeiou".contains($0) }.count
+    }
+    private func halves(of str: String) -> (Substring, Substring) {
+        return (str[..<str.index(str.startIndex, offsetBy: str.count/2)], str[str.index(str.startIndex, offsetBy: str.count/2)...])
+    }
+    func halvesAreAlike(_ s: String) -> Bool {
+        let splitted = halves(of: s.lowercased())
+        return countVowels(in: splitted.0) == countVowels(in: splitted.1)
+    }
+}
