60e56a40b022dfad51a7051974232a18a401288d
loyihsu <loyi.hsu@gmail.com>
2020-12-29 03:42:25 +0800

    Create 1652.

---
+++ Solutions/01 - Easy/1652. Defuse the Bomb.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/defuse-the-bomb/
+
+class Solution {
+    func decrypt(_ code: [Int], _ k: Int) -> [Int] {
+        var temp = code
+        for idx in temp.indices {
+            var sum = 0
+            for offset in k >= 0 ? 0..<k : k..<0 {
+                let constant = k >= 0 ? 1 : code.count
+                sum += code[(idx + offset + constant)%code.count]
+            }
+            temp[idx] = sum            
+        }
+        return temp
+    }
+}
