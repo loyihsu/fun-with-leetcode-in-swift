e0547adcb4542650df47e82ea5481c148a0bde21
loyihsu <loyi.hsu@gmail.com>
2020-12-29 04:15:57 +0800

    Create 1598.

---
+++ Solutions/01 - Easy/1598. Crawler Log Folder.swift
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/crawler-log-folder/
+
+class Solution {
+    func minOperations(_ logs: [String]) -> Int {
+        var level = 0
+        for log in logs {
+            if log == "../" {
+                level = level - 1 >= 0 ? level - 1 : level
+            } else if log != "./" {
+                level += 1
+            }
+        }
+        return level
+    }
+}
