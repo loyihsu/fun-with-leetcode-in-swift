d470b95478faafcf0a9c58bf703de233a0dfa027
loyihsu <loyi.hsu@gmail.com>
2021-01-07 14:39:45 +0800

    Create 1710.

---
+++ Solutions/01 - Easy/1710. Maximum Units on a Truck.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/maximum-units-on-a-truck/
+
+class Solution {
+    func maximumUnits(_ boxTypes: [[Int]], _ truckSize: Int) -> Int {
+        var boxes = boxTypes.map({ ($0[0], $0[1]) }).sorted {
+            $0.1 > $1.1
+        }
+        var output = (0, 0), idx = 0
+        while output.0 < truckSize && idx < boxes.count {
+            output.0 += 1
+            output.1 += boxes[idx].1
+            boxes[idx].0 -= 1
+            if boxes[idx].0 == 0 {
+                idx += 1
+            }
+        }
+        return output.1
+    }
+}
