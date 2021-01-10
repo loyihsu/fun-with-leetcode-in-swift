894a13ae500d30d18eebd2abeebf08b24a9544ec
loyihsu <loyi.hsu@gmail.com>
2021-01-11 00:47:48 +0800

    Create 1716.

---
+++ Solutions/01 - Easy/1716. Calculate Money in Leetcode Bank.swift
@@ -0,0 +1,11 @@
+// Problem: https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+
+class Solution {
+    func totalMoney(_ n: Int) -> Int {
+        var output = 0
+        for count in 0..<n {
+            output += count % 7 + 1 + count / 7
+        }
+        return output
+    }
+}
