d318ad1b1d88c98e371a28e9f21b1e8f014cfe44
loyihsu <loyi.hsu@gmail.com>
2021-01-14 00:43:57 +0800

    Create 1523.

---
+++ Solutions/01 - Easy/1523. Count Odd Numbers in an Interval Range.swift
@@ -0,0 +1,7 @@
+// Problem: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+class Solution {
+    func countOdds(_ low: Int, _ high: Int) -> Int {
+        return (high - low + 1) / 2 + ((high - low + 1) % 2 != 0 && low % 2 == 1 ? 1 : 0)
+    }
+}
