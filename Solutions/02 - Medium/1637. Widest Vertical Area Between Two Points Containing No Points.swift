8bf594a0764b3bf9dad553c14d2ddef7459318aa
loyihsu <loyi.hsu@gmail.com>
2021-01-15 21:28:16 +0800

    Create 1637.

---
+++ Solutions/02 - Medium/1637. Widest Vertical Area Between Two Points Containing No Points.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+
+class Solution {
+    func maxWidthOfVerticalArea(_ points: [[Int]]) -> Int {
+        let sortedYs = Array(Set(points.map { $0[0] })).sorted()
+        var max = Int.min
+        guard sortedYs.count > 1 else { return 0 }
+        for idx in 1..<sortedYs.count {
+            let temp = sortedYs[idx] - sortedYs[idx-1]
+            if temp > max {
+                max = temp
+            }
+        }
+        return max
+    }
+}
