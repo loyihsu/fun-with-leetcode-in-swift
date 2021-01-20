11a826801763a98c16ffd18ee91444dca11e7daa
loyihsu <loyi.hsu@gmail.com>
2021-01-20 11:41:22 +0800

    Create 1266.

---
+++ Solutions/01 - Easy/1266. Minimum Time Visiting All Points.swift
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+class Solution {
+    func minTimeToVisitAllPoints(_ points: [[Int]]) -> Int {
+        var pos = (points[0][0], points[0][1]), res = 0
+        for idx in 1..<points.count {
+            while points[idx][0] != pos.0 || points[idx][1] != pos.1 {
+                if pos.0 < points[idx][0] {
+                    pos.0 += 1
+                } else if pos.0 > points[idx][0] {
+                    pos.0 -= 1
+                }
+                if pos.1 < points[idx][1] {
+                    pos.1 += 1
+                } else if pos.1 > points[idx][1] {
+                    pos.1 -= 1
+                }
+                res += 1
+            }
+        }
+        return res
+    }
+}
