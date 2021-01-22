85273aba1834a20ecac0393142373599d01a2e68
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-01-23 03:14:01 +0800

    Create 0168.

---
+++ Solutions/01 - Easy/0168. Excel Sheet Column Title.swift
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/excel-sheet-column-title/
+
+class Solution {
+    var map = [Int: Character]()
+    init() {
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".enumerated().forEach {
+            map[$0] = $1
+        }
+    }
+    func helper(_ number: Int) -> String {
+        return "\(map[number - 1 >= 0 ? number - 1 : 25]!)"
+    }
+    func convertToTitle(_ n: Int) -> String {
+        var temp = n, output = ""
+        while temp > 0 {
+            let current = temp % 26
+            output = "\(helper(current))\(output)"
+            temp = (temp - 1) / 26
+        }
+        return output
+    }
+}
