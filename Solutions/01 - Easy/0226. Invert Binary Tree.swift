2862b3238e0bd2b933bc19022ed33d43d3d7f3cf
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-01-26 15:39:03 +0800

    Create 0226.

---
+++ Solutions/01 - Easy/0226. Invert Binary Tree.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else { return nil }
+        
+        if let left = root.left {
+            root.left = invertTree(left)
+        }
+        if let right = root.right {
+            root.right = invertTree(right)
+        }
+        var temp = root.right
+        root.right = root.left
+        root.left = temp
+        return root
+    }
+}
