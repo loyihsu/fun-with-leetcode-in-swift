4b7406520927bd0445b9c4ab27a981d781b2a723
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-02-09 00:10:52 +0800

    Create 1417.

---
+++ Solutions/01 - Easy/1417. Reformat The String.swift
@@ -0,0 +1,34 @@
+// Problem: https://leetcode.com/problems/reformat-the-string/
+
+class Solution {
+    func reformat(_ s: String) -> String {
+        var digits = "", chars = ""
+        for char in s {
+            if "0123456789".contains(char) {
+                digits.append(char)
+            } else {
+                chars.append(char)
+            }
+        }
+        if digits.count == chars.count || digits.count == chars.count + 1 || digits.count == chars.count - 1 {
+            var output = ""
+            while !digits.isEmpty && !chars.isEmpty {
+                if chars.count > digits.count{
+                    output.append(chars.popLast()!)
+                    output.append(digits.popLast()!)
+                } else {
+                    output.append(digits.popLast()!)
+                    output.append(chars.popLast()!)
+                }
+            }
+            if !digits.isEmpty {
+                output.append(digits.popLast()!)
+            }
+            if !chars.isEmpty {
+                output.append(chars.popLast()!)
+            }
+            return output
+        }
+        return ""
+    }
+}
