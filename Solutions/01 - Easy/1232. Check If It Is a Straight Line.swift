e32ab2041a47827f739cd186ee9fadc19b63290f
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-02-09 00:10:47 +0800

    Create 1232.

---
+++ Solutions/01 - Easy/1232. Check If It Is a Straight Line.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/check-if-it-is-a-straight-line/
+
+class Solution {
+    func checkStraightLine(_ coordinates: [[Int]]) -> Bool {
+        guard coordinates.count > 2 else { return true }
+        var previous = Double(coordinates[1][1] - coordinates[0][1])/Double(coordinates[1][0] - coordinates[0][0])
+        for idx in 2..<coordinates.count {
+            let current = Double(coordinates[idx][1] - coordinates[idx-1][1])/Double(coordinates[idx][0] - coordinates[idx-1][0])
+            if abs(previous) == Double.infinity && abs(previous) == abs(current) {
+                continue
+            }
+            if previous != current { return false }
+        }
+        return true
+    }
+}
