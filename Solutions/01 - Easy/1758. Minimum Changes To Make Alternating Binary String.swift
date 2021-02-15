277d7d7ee6941e5f77a6226a27fb0b15b15dc2d1
loyihsu <loyi.hsu@gmail.com>
2021-02-15 18:45:38 +0800

    Create 1758.

---
+++ Solutions/01 - Easy/1758. Minimum Changes To Make Alternating Binary String.swift
@@ -0,0 +1,29 @@
+// Problem: https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+
+class Solution {
+    func minOperations(_ s: String) -> Int {
+        var counterA = 0, counterB = 0
+        var signalA = false, signalB = true
+        
+        for item in s {
+            if item == "1" {
+                if signalA == false {
+                    counterA += 1
+                }
+                if signalB == false {
+                    counterB += 1
+                }
+            } else {
+                if signalA == true {
+                    counterA += 1
+                }
+                if signalB == true {
+                    counterB += 1
+                }
+            }
+            signalA = !signalA
+            signalB = !signalB
+        }
+        return counterA > counterB ? counterB : counterA
+    }
+}
