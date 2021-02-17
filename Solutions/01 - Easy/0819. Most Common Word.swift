c3c1bc97c07b20609fe4dae649cb24f55499faf9
loyihsu <loyi.hsu@gmail.com>
2021-02-17 15:42:39 +0800

    Create 0819.

---
+++ Solutions/01 - Easy/0819. Most Common Word.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/most-common-word/
+
+class Solution {
+    func mostCommonWord(_ paragraph: String, _ banned: [String]) -> String {
+        var testcase = paragraph.lowercased().components(separatedBy: .whitespaces), map = [String: Int]()
+        for word in testcase {
+            var word = word
+            word = word.components(separatedBy: .punctuationCharacters).first!
+            map[word, default: 0] += 1
+        }
+        
+        for (key, value) in map.sorted { $0.value > $1.value } where !banned.contains(key) {
+            return key
+        }
+        return ""
+    }
+}
