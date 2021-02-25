f8252101b8d472a9c0b97e8b5531e86bdcc9ee44
loyihsu <loyi.hsu@gmail.com>
2021-02-25 16:59:55 +0800

    Create 0201.

---
+++ Solutions/02 - Medium/0201. Bitwise AND of Numbers Range.swift
@@ -0,0 +1,11 @@
+// Problem: https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution {
+    func rangeBitwiseAnd(_ m: Int, _ n: Int) -> Int {
+        var n = n
+        while (m < n) {
+            n &= (n-1)
+        }
+        return n
+    }
+}
