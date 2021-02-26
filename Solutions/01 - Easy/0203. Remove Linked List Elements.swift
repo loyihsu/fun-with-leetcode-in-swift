7b9e22939e3ad1825bcdd461efc9f8837d2039fd
loyihsu <loyi.hsu@gmail.com>
2021-02-26 20:59:37 +0800

    Create 0203.

---
+++ Solutions/01 - Easy/0203. Remove Linked List Elements.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/remove-linked-list-elements/
+
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        var temp = head, newList: ListNode?, last: ListNode?
+        while let element = temp {
+            if element.val != val {
+                let newNode = ListNode(element.val)
+                if let last = last {
+                    last.next = newNode
+                } else {
+                    newList = newNode
+                }
+                last = newNode
+            }
+            temp = element.next
+        }
+        return newList
+    }
+}
