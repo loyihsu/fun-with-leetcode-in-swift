39094c38d7ae15888d43660bc806df3a1bd90248
loyihsu <loyi.hsu@gmail.com>
2021-03-01 14:36:41 +0800

    Create 0205.

---
+++ Solutions/01 - Easy/0205. Isomorphic Strings.swift
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/isomorphic-strings/
+
+class Solution {
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        var table = [Character: String]()
+        var nextChar = 1, processedStrings = ("", "")
+        for char in s {
+            if table[char] == nil {
+                table[char] = String(nextChar)
+                nextChar += 1
+            }
+            processedStrings.0 += table[char, default: " "]
+        }
+        
+        nextChar = 1
+        table = [Character: String]()
+        for char in t {
+            if table[char] == nil {
+                table[char] = String(nextChar)
+                nextChar += 1
+            }
+            processedStrings.1 += table[char, default: " "]
+        }
+        return processedStrings.0 == processedStrings.1
+    }
+}
