88d2073eca5290c9633d0e53a07f97c1a374b63f
loyihsu <loyi.hsu@gmail.com>
2021-03-03 20:19:33 +0800

    Create 0507.

---
+++ Solutions/01 - Easy/0507. Perfect Number.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/perfect-number/
+
+class Solution {
+    func checkPerfectNumber(_ num: Int) -> Bool {
+        guard num != 1 else { return false }
+        var sum = -num
+        for idx in 1...Int(sqrt(Double(num))) where num % idx == 0 {
+            sum += idx
+            sum += num/idx
+            if (sum > num) {
+                return false
+            }   
+        }
+        return sum == num
+    }
+}
