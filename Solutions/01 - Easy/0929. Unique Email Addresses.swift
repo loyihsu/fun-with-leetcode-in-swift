ef0f5cd6e2ef0883aa5f5cb84856710c53509d92
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-03-07 08:39:17 +0800

    Update 0929.

--- Solutions/01 - Easy/0929. Unique Email Addresses.swift
+++ Solutions/01 - Easy/0929. Unique Email Addresses.swift
@@ -1,14 +1,27 @@
 // Problem: https://leetcode.com/problems/unique-email-addresses/
 
 class Solution {
-    func numUniqueEmails(_ emails: [String]) -> Int {
-        var output = [String: Bool]()
-        for email in emails {
-            var atSeparator = email.components(separatedBy: "@")
-            atSeparator[0] = atSeparator[0].components(separatedBy: "+")[0]
-            atSeparator[0] = atSeparator[0].filter { $0 != "." }
-            output[atSeparator.joined(separator: "@")] = true
+    func parse(_ str: String, separator: Character) -> [String] {
+        var output: [String] = [], temp = ""
+        for char in str {
+            if char != separator {
+                temp.append(char)
+            } else {
+                output.append(temp)
+                temp = ""
+            }
         }
-        return output.keys.count
+        output.append(temp)
+        return output
+    }
+    func numUniqueEmails(_ emails: [String]) -> Int {
+        var output = Set<String>()
+        for email in emails {
+            var parsed = parse(email, separator: "@")
+            parsed[0] = parse(parsed[0], separator: "+")[0]
+            parsed[0] = parsed[0].filter { $0 != "." }
+            output.insert(parsed[0] + "@" + parsed[1])
+        }
+        return output.count
     }
 }
