c941ce5e77e2614ad786497e6535cde8b2fa1f7e
loyihsu <loyi.hsu@gmail.com>
2021-03-08 22:53:40 +0800

    Create 0303.

---
+++ Solutions/01 - Easy/0303. Range Sum Query - Immutable.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/range-sum-query-immutable/
+
+class NumArray {
+    var sums: [Int]
+    init(_ nums: [Int]) {
+        var temp = 0
+        sums = [Int](repeating: 0, count: nums.count)
+        for idx in nums.indices {
+            temp += nums[idx]
+            sums[idx] = temp
+        }
+    }   
+    func sumRange(_ i: Int, _ j: Int) -> Int {
+        sums[j] - (i != 0 ? sums[i-1] : 0)
+    }
+}
