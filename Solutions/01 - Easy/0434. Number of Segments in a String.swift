00c6606862b15ed339d0a99aecada715d0ba5fe0
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-03-19 15:53:13 +0800

    Create 0434.

---
+++ Solutions/01 - Easy/0434. Number of Segments in a String.swift
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/number-of-segments-in-a-string/
+
+class Solution {
+    func countSegments(_ s: String) -> Int {
+        var temp = false, count = 0
+        for char in s {
+            if char == " " {
+                if temp {
+                    temp = false
+                    count += 1
+                } 
+            } else {
+                temp = true
+            }
+        }
+        if temp {
+            count += 1
+        }
+        return count
+    }
+}
