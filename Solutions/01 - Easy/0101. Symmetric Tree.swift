ffb025093251375d1934dea3a4acff5cb8f64423
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-03-27 04:51:46 +0800

    Create 0101.

---
+++ Solutions/01 - Easy/0101. Symmetric Tree.swift
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/symmetric-tree/
+
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return root?.left == root?.right
+    }
+}
+extension TreeNode: Equatable {
+    public static func ==(lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return lhs.val == rhs.val && lhs.left == rhs.right && rhs.left == lhs.right
+    }
+}
