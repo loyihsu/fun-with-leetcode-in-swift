51f55666458b39ffc8013cd9d56fb97ad63a71f4
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-04-02 15:58:07 +0800

    Create 0572.

---
+++ Solutions/01 - Easy/0572. Subtree of Another Tree.swift
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/subtree-of-another-tree/
+
+class Solution {
+    func isSubtree(_ s: TreeNode?, _ t: TreeNode?) -> Bool {
+        guard let left = s, let right = t else { return s == nil && t == nil }
+        return isEqual(left, right) || isSubtree(left.left, right) || isSubtree(left.right, right)
+    }
+    func isEqual(_ s: TreeNode?, _ t: TreeNode?) -> Bool {
+        guard let left = s, let right = t else { return s == nil && t == nil }
+        return left.val == right.val && isEqual(left.left, right.left) && isEqual(left.right, right.right) 
+    }
+}
