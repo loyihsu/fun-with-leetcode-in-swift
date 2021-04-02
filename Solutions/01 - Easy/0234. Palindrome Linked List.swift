452aaea530cbc47e6997733ad2534441da89a8c0
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-04-02 12:37:46 +0800

    Create 0234.

---
+++ Solutions/01 - Easy/0234. Palindrome Linked List.swift
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/palindrome-linked-list/
+
+class Solution {
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var arr = [Int](), temp = head
+        while let cur = temp {
+            arr.append(cur.val)
+            temp = cur.next
+        }
+        var left = 0, right = arr.count - 1
+        while left < right {
+            if arr[left] != arr[right] {
+                return false
+            }
+            (left, right) = (left+1,right-1)    
+        }
+        return true
+    }
+}
