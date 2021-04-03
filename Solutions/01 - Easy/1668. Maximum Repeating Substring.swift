59e5d70709998c508318cbbe8e8fd864b28a51c0
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-04-04 02:35:38 +0800

    Create 1668.

---
+++ Solutions/01 - Easy/1668. Maximum Repeating Substring.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/maximum-repeating-substring/
+
+class Solution {
+    func maxRepeating(_ sequence: String, _ word: String) -> Int {
+        var count = 0
+        let max = sequence.count/word.count
+        guard max > 0 else { return 0 }
+        for num in 1...max where sequence.contains(String(repeating: word, count: num)) {
+            count = num
+        }
+        return count
+    }
+}
