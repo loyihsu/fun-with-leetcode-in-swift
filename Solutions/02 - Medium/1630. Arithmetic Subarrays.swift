d95b25c24e9838c0e30fce1cd488be327254f049
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-04-06 00:16:17 +0800

    Create 1630.

---
+++ Solutions/02 - Medium/1630. Arithmetic Subarrays.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/arithmetic-subarrays/
+
+class Solution {
+    func checkArithmetic(_ arr: [Int]) -> Bool {
+        guard arr.count > 2 else { return true }
+        let arr = arr.sorted()
+        let diff = arr[1] - arr[0]
+        for idx in 2..<arr.count where arr[idx] - arr[idx-1] != diff {
+            return false           
+        }
+        return true
+    }
+    func checkArithmeticSubarrays(_ nums: [Int], _ l: [Int], _ r: [Int]) -> [Bool] {
+        var output = [Bool](repeating: false, count: l.count)
+        for idx in l.indices {
+            output[idx] = checkArithmetic(Array(nums[l[idx]...r[idx]]))
+        }
+        return output
+    }
+}
