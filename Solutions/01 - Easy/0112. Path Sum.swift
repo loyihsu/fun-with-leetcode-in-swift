063a7c702732a3da8a270d50cdffcc1e20530a60
loyihsu <loyi.hsu@gmail.com>
2021-04-12 19:48:12 +0800

    Create 0112.

---
+++ Solutions/01 - Easy/0112. Path Sum.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/path-sum/
+
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root = root else { return false }
+        var stack: [(node: TreeNode, left: Int)] = [(root, targetSum)]
+        while let cur = stack.popLast() {
+            if cur.node.left == nil && cur.node.right == nil && cur.left == cur.node.val {
+                return true
+            }
+            for child in [cur.node.left, cur.node.right].compactMap({$0}) {
+                stack.append((child, cur.left - cur.node.val))
+            }
+        }
+        return false
+    }
+}
