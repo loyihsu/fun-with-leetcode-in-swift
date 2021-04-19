1c40de9b8bd7c85417fbb79eae88592e934b7a21
loyihsu <loyi.hsu@gmail.com>
2021-04-19 18:39:03 +0800

    Create 0605 & 0859.

---
+++ Solutions/01 - Easy/0605. Can Place Flowers.swift
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/can-place-flowers/
+
+class Solution {
+    func checkAndPlace(_ flowerbed: inout [Int], idx: Int) -> Bool {
+        var flag = true
+        if idx - 1 >= 0 && flowerbed[idx-1] == 1 {
+            flag = false
+        }
+        if idx + 1 < flowerbed.count && flowerbed[idx+1] == 1 {
+            flag = false
+        }
+        if flag {
+            flowerbed[idx] = 1
+        }
+        return flag
+    }
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        var n = n, flowerbed = flowerbed
+        for idx in flowerbed.indices where flowerbed[idx] == 0 && n > 0 {
+            if checkAndPlace(&flowerbed, idx: idx) {
+                n -= 1
+            }
+        }
+        return n == 0
+    }
+}
---
+++ Solutions/01 - Easy/0859. Buddy Strings.swift
@@ -0,0 +1,28 @@
+// Problem: https://leetcode.com/problems/buddy-strings/
+
+class Solution {
+    func atLeastOneBiggerThanTwo(_ str: [Character]) -> Bool {
+        var map = [Character: Int]()
+        str.forEach { map[$0, default: 0] += 1 }
+        return map.contains(where: { (_, val) in val > 1 })
+    }
+    func buddyStrings(_ a: String, _ b: String) -> Bool {
+        guard a.count > 1 else { return false }
+        guard Set(a).count > 1 || a != b else { return true }
+        guard a.count == b.count else { return false }
+        var a = Array(a), b = Array(b), count = 0, stack = ""
+        for idx in a.indices where a[idx] != b[idx] {
+            if stack.isEmpty {
+                stack = "\(a[idx])\(b[idx])"
+            } else if stack.first == b[idx] && stack.last == a[idx] {
+                stack = ""
+            }
+            if count < 2 {
+                count += 1
+            } else {
+                return false
+            }
+        }
+        return count == 2 && stack.isEmpty || count == 0 && Set(a).count > 1 && a == b && (atLeastOneBiggerThanTwo(a))
+    }
+}
