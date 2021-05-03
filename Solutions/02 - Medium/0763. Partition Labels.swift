2e24d7063c226d48249a2f067466221c7f412cfd
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-05-03 16:30:40 +0800

    Create 0763.

---
+++ Solutions/02 - Medium/0763. Partition Labels.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/partition-labels/
+
+class Solution {
+    func partitionLabels(_ S: String) -> [Int] {
+        var map = [Character:Int](), output = [Int]()
+        var start = 0, end = 0
+        S.enumerated().forEach({map[$1, default: 0] = $0})
+        for (idx, char) in S.enumerated() {
+            end = max(end, map[char, default: 0])
+            if idx != end { continue }
+            output.append(idx - start + 1)
+            start = idx + 1
+        }
+        return output
+    }
+}
