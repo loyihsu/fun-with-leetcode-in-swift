a644c311e1d64f1e2a1c3799b27e77eb5307ce4c
loyihsu <loyi.hsu@gmail.com>
2021-05-12 14:10:33 +0800

    Create 0228.

---
+++ Solutions/01 - Easy/0228. Summary Ranges.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/summary-ranges/
+
+class Solution {
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        guard let first = nums.first else { return [] }
+        guard nums.count > 1 else { return ["\(first)"]}
+        var prev = first, start = first, output = [String]()
+        for idx in 1..<nums.count {
+            if nums[idx] != prev + 1 {
+                output.append(start != prev ? "\(start)->\(prev)" : "\(start)")                    
+                start = nums[idx]
+            }
+            prev = nums[idx]
+        }
+        output.append(start != prev ? "\(start)->\(prev)" : "\(start)")
+        return output
+    }
+}
