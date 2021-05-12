9638de988c5ae2fafb822cc0ab22cb8b356b2ef2
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-05-13 03:13:16 +0800

    Create 0048.

---
+++ Solutions/02 - Medium/0048. Rotate Image.swift
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/rotate-image/
+
+class Solution {
+    func rotate(_ matrix: inout [[Int]]) {
+        var temp = 0, first = 0, second = 0
+        for idx in 0..<matrix.count/2 {
+            for jdx in idx..<matrix.count-idx-1 {
+                temp = matrix[idx][jdx]
+                (first, second) = (jdx, matrix.count-idx-1)
+                (matrix[first][second], temp) = (temp, matrix[first][second])
+                (first, second) = (second, matrix.count-first-1)
+                (matrix[first][second], temp) = (temp, matrix[first][second])
+                (first, second) = (second, matrix.count-first-1)
+                (matrix[first][second], temp) = (temp, matrix[first][second])
+                (first, second) = (second, matrix.count-first-1)
+                (matrix[first][second], temp) = (temp, matrix[first][second])
+            }
+        }
+    }
+}
