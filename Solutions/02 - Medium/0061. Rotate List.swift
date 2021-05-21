2ee30de4bf71c241725c26a757be932ab98779a6
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-05-22 01:38:28 +0800

    Create 0061.

---
+++ Solutions/02 - Medium/0061. Rotate List.swift
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/rotate-list/
+
+class Solution {
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        guard head != nil && k > 0 else { return head }
+        var list = [Int](), temp = head, last = head, counter = 0
+        while let current = temp {
+            list.append(current.val)
+            temp = current.next
+        }
+        let change = k % list.count
+        guard change > 0 else { return head }
+        temp = head
+        while last?.next != nil {
+            if counter >= change {
+                temp = temp?.next
+            }
+            last = last?.next
+            counter += 1
+        }
+        let newHead = temp?.next
+        temp?.next = nil
+        last?.next = head
+        return newHead
+    }
+}
