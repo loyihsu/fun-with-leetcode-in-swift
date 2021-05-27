bf88476d0938937a67338479907ff68b44e849a6
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-05-27 21:38:49 +0800

    Create 1854.

---
+++ Solutions/01 - Easy/1854. Maximum Population Year.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/maximum-population-year/
+
+class Solution {
+    func maximumPopulation(_ logs: [[Int]]) -> Int {
+        var years = [Int](repeating: 0, count: 101)
+        var max: (idx: Int, count: Int) = (-1, -1)
+        for log in logs {
+            for idx in (log[0]-1949)..<(log[1]-1949) {
+                years[idx] += 1
+            }
+        }
+        for (idx, count) in years.enumerated() where count > max.count {
+                max = (idx, count)
+        }
+        return max.idx + 1949
+    }
+}
