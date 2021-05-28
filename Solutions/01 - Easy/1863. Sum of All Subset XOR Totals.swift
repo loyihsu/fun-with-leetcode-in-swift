530b3a91dfd3997738b88f535dbfc100f074636a
loyihsu <loyi.hsu@gmail.com>
2021-05-29 00:58:22 +0800

    Create 1863.

---
+++ Solutions/01 - Easy/1863. Sum of All Subset XOR Totals.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+
+class Solution {
+    func subsetXORSum(_ nums: [Int]) -> Int {
+        return allSubsets(nums, cur: 0, xor: 0)
+    }
+    func allSubsets(_ nums: [Int], cur: Int, xor: Int) -> Int {
+        guard cur != nums.count else { return xor }
+        let include = allSubsets(nums, cur: cur + 1, xor: nums[cur] ^ xor)
+        let exclude = allSubsets(nums, cur: cur + 1, xor: xor)
+        return include + exclude
+    }
+}
