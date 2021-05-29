f13bd8cab4257feb6cf52bb1e0dde0f34eb2dd4b
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-05-30 02:36:00 +0800

    Update 1451.

--- Solutions/02 - Medium/1451. Rearrange Words in a Sentence.swift
+++ Solutions/02 - Medium/1451. Rearrange Words in a Sentence.swift
@@ -5,7 +5,7 @@ class Solution {
         var components = text.components(separatedBy: .whitespaces)
             .filter({ $0.isEmpty == false })
             .map({ $0.lowercased() })
-        components.sort({ $0.count < $1.count })
+        components.sort(by: { $0.count < $1.count })
         var first = components[0].removeFirst().uppercased()
         components[0].insert(contentsOf: first, at: components[0].startIndex)
         return components.joined(separator: " ")
