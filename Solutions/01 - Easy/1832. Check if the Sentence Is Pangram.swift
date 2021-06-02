8eeb106fdbca3e9f47f458cc4c49cb7efd86c9e8
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-06-02 18:21:12 +0800

    Create 1832.

---
+++ Solutions/01 - Easy/1832. Check if the Sentence Is Pangram.swift
@@ -0,0 +1,7 @@
+// Problem: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+class Solution {
+    func checkIfPangram(_ sentence: String) -> Bool {
+        return Set(sentence).count == 26
+    }
+}
