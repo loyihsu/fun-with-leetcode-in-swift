f6e5b4c230bb346d43a114d6f319a65e9d7fc8b4
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-06-11 15:33:19 +0800

    Create 1844.

---
+++ Solutions/01 - Easy/1844. Replace All Digits with Characters.swift
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/replace-all-digits-with-characters/
+
+class Solution {
+    private func shift(_ character: Character, by offset: Int) -> Character {
+        let code = character.unicodeScalars.first!.value
+        var value = Int(code) + offset
+        if value > "z".unicodeScalars.first!.value {
+            value -= 26
+        }
+        return Character(UnicodeScalar(value)!)
+    }
+    func replaceDigits(_ str: String) -> String {
+        var output = ""
+        for (idx, char) in str.enumerated() {
+            if idx % 2 == 0 {
+                output.append(char)
+            } else {
+                output.append(shift(output.last!, by: Int("\(char)")!))
+            }
+        }
+        return output
+    }
+}
