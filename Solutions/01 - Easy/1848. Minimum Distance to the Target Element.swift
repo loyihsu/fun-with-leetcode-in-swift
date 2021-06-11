9f21fdba3e601b82b819f613b103ce3fc915b2c6
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-06-11 15:16:56 +0800

    Create 1848.

---
+++ Solutions/01 - Easy/1848. Minimum Distance to the Target Element.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/minimum-distance-to-the-target-element/
+
+class Solution {
+    func getMinDistance(_ nums: [Int], _ target: Int, _ start: Int) -> Int {
+        guard nums[start] != target else { return 0 }
+        var output = Int.max
+        for idx in nums.indices where nums[idx] == target {
+            let val = abs(start-idx)
+            output = output > val ? val : output
+        }
+        return output
+    }
+}
