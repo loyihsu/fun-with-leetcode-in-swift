99f16443c1b68bd5db5bf7d900d5e1aa4630780a
Yu-Sung Loyi Hsu <loyi.hsu@gmail.com>
2021-06-13 08:17:49 +0800

    Create 1893.

---
+++ Solutions/01 - Easy/1893. Check if All the Integers in a Range Are Covered.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/ 
+
+class Solution {
+    func isCovered(_ ranges: [[Int]], _ left: Int, _ right: Int) -> Bool {
+        let set: Set<Int> = Set(ranges.flatMap({ Array($0[0]...$0[1]) }))
+        for num in left...right {
+            if !set.contains(num) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
