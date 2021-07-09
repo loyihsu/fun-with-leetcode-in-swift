80613e18d7afe222df7dfd7cbbb77aa5d2d79d2c
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-09 19:00:31 +0800

    Create 1800.

---
+++ Solutions/01 - Easy/1800. Maximum Ascending Subarray Sum.swift
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/maximum-ascending-subarray-sum/
+
+class Solution {
+    func maxAscendingSum(_ nums: [Int]) -> Int {
+        guard nums.count != 1 else { return nums[0] }
+        var subs = [ArraySlice<Int>](), start = 0, prev = 0
+        for idx in 1..<nums.count {
+            if nums[idx] <= nums[idx-1] {
+                subs.append(nums[prev..<idx])
+                prev = idx
+            }
+        }
+        subs.append(nums[prev..<nums.count])
+        return subs.flatMap({$0.reduce(0, +)}).max()!
+    }
+}
