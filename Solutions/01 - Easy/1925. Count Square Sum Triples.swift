ec8521e9fef0411a410b1db6c916161ccfea90f7
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-13 01:43:00 +0800

    Create 1925.

---
+++ Solutions/01 - Easy/1925. Count Square Sum Triples.swift
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/count-square-sum-triples/
+
+import Foundation
+
+class Solution {
+    func isInteger(_ double: Double) -> Bool {
+        return Double(Int(double)) == double
+    }
+    func countTriples(_ n: Int) -> Int {
+        var count = 0
+        for idx in 1...n {
+            for jdx in idx...n {
+                let double: Double = sqrt(Double(idx*idx + jdx*jdx))
+                if Int(double) > n {
+                    break
+                }
+                let change = idx == jdx ? 1 : 2
+                count = isInteger(double) ? count + change : count
+            }
+        }
+        return count
+    }
+}
