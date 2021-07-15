cbe4d21fa5c11a44ba14004fac2f5d0b1504bbf0
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-15 20:22:48 +0800

    Upgrade 1551 with math.

--- Solutions/02 - Medium/1551. Minimum Operations to Make Array Equal.swift
+++ Solutions/02 - Medium/1551. Minimum Operations to Make Array Equal.swift
@@ -2,14 +2,6 @@
 
 class Solution {
     func minOperations(_ n: Int) -> Int {
-        var target = 0, operations = 0
-        for idx in 0..<n {
-            target += idx * 2 + 1
-        }
-        target = target / n
-        for idx in 0..<n/2 {
-            operations += target - (idx * 2 + 1)
-        }
-        return operations
+        return n * n / 4
     }
 }
