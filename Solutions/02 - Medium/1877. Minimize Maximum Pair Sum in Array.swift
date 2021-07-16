62f8a54aaf2c563a702574c5a6ed99c17da50363
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-16 19:12:44 +0800

    Create 1877.

---
+++ Solutions/02 - Medium/1877. Minimize Maximum Pair Sum in Array.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+
+class Solution {
+    func minPairSum(_ nums: [Int]) -> Int {
+        let sorted = nums.sorted()
+        var idx = 1, jdx = nums.count-2, pairSum = sorted[0] + sorted[nums.count-1]
+        while idx < jdx {
+            pairSum = sorted[idx] + sorted[jdx] > pairSum ? sorted[idx] + sorted[jdx] : pairSum
+            (idx, jdx) = (idx + 1, jdx - 1)
+        }
+        return pairSum
+    }
+}
