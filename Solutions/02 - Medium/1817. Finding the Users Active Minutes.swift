8133acc04ef9b37b345a6cfe541a22b156f681bb
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-23 02:27:38 +0800

    Create 1817.

---
+++ Solutions/02 - Medium/1817. Finding the Users Active Minutes.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/finding-the-users-active-minutes/
+
+class Solution {
+    func findingUsersActiveMinutes(_ logs: [[Int]], _ k: Int) -> [Int] {
+        var userLogs = [Int: Set<Int>](), output = [Int](repeating: 0, count: k)
+        for log in logs {
+            userLogs[log[0], default: []].insert(log[1])
+        }
+        for value in userLogs.values {
+            output[value.count-1] += 1
+        }
+        return output
+    }
+}
