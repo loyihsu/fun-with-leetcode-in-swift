1efea4abefb27f2ed0468b3e7105d0232bdaa8d8
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-07-31 01:50:29 +0800

    Create 1286.

---
+++ Solutions/02 - Medium/1286. Iterator for Combination.swift
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/iterator-for-combination/
+
+class CombinationIterator {
+    var storage = [String]()
+    init(_ characters: String, _ combinationLength: Int) {
+        preprocessing("", characters, length: combinationLength)
+        storage.sort()
+    }
+    private func preprocessing(_ current: String, _ resting: String, length: Int) {
+        if current.count == length {
+            storage.append(current)
+        } else if !resting.isEmpty {
+            var rest = resting, first = rest.removeFirst()
+            preprocessing(current, rest, length: length)
+            preprocessing("\(current)\(first)", rest, length: length)
+        }
+    }
+    func next() -> String {
+        return storage.removeFirst()
+    }
+    func hasNext() -> Bool {
+        return !storage.isEmpty
+    }
+}
