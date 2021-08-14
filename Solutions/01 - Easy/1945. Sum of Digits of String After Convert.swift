101d88ff188ce179d9f3a3938595eae44ee3ab17
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-08-14 15:44:46 +0800

    Create 1945.

---
+++ Solutions/01 - Easy/1945. Sum of Digits of String After Convert.swift
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+
+class Solution {
+    func getLucky(_ s: String, _ k: Int) -> Int {
+        let pos = Array(" abcdefghijklmnopqrstuvwxyz")
+        let digits = Array("0123456789")
+        var cur = "", count = 0
+        for char in s {
+            let temp = "\(pos.firstIndex(of: char)!)"
+            cur.append(temp)
+        }
+        for _ in 0..<k {
+            count = 0
+            for digit in cur {
+                count += digits.firstIndex(of: digit)!
+            }
+            cur = "\(count)"
+        }
+        return Int(cur)!
+    }
+}
