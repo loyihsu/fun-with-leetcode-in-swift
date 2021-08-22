3cd85eb00b788485f8d0bbcfdbfe9d416baad50f
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-08-22 17:58:26 +0800

    Improve 0232 with LinkedList.

--- Solutions/01 - Easy/0232. Implement Queue using Stacks.swift
+++ Solutions/01 - Easy/0232. Implement Queue using Stacks.swift
@@ -1,24 +1,53 @@
 // Problem: https://leetcode.com/problems/implement-queue-using-stacks/
 
+class ListNode {
+    var value: Int
+    var next: ListNode?
+    init(_ value: Int, next: ListNode? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+class LinkedStack {
+    var top: ListNode? = nil
+    func push(_ val: Int) {
+        let newNode = ListNode(val)
+        if top != nil {
+            newNode.next = top
+        }
+        top = newNode
+    }
+    func pop() -> Int {
+        let val = top!.value
+        top = top?.next
+        return val
+    }
+    func peek() -> Int {
+        return top!.value
+    }
+    var isEmpty: Bool {
+        return top == nil
+    }
+}
 class MyQueue {
-    var left = [Int](), right = [Int]()     // (left-top)[left][right](right-top)
+    var left = LinkedStack(), right = LinkedStack() // (left-top)[left][right](right-top)
     /** Push element x to the back of queue. */
     func push(_ x: Int) {
-        right.append(x)
+        right.push(x)
     }
     /** Removes the element from in front of queue and returns that element. */
     func pop() -> Int {
         if left.isEmpty {
             charge()
         }
-        return left.popLast()!
+        return left.pop()
     }
     /** Get the front element. */
     func peek() -> Int {
         if left.isEmpty {
             charge()
         }
-        return left.last!
+        return left.peek()
     }
     /** Returns whether the queue is empty. */
     func empty() -> Bool {
@@ -26,7 +55,7 @@ class MyQueue {
     }
     private func charge() {
         while !right.isEmpty {
-            left.append(right.popLast()!)
+            left.push(right.pop())
         }
     }
 }
