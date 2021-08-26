7b614e8f17b3aebbab63705d350741c3ebdd625d
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-08-26 16:23:26 +0800

    Create 1935.

---
+++ Solutions/01 - Easy/1935. Maximum Number of Words You Can Type.swift
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+
+class Solution {
+    func canBeTypedWords(_ text: String, _ brokenLetters: String) -> Int {
+        let broken = Set(brokenLetters)
+        return text
+            .components(separatedBy: " ")
+            .map({ Set($0).intersection(broken) })
+            .filter({ $0.isEmpty })
+            .count
+    }
+}
