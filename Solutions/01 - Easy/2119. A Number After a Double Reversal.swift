02edd9cab8d2de8d43df8c45f7290c8cc7d583ba
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-12-27 21:25:09 +0800

    Create 2119.

---
+++ Solutions/01 - Easy/2119. A Number After a Double Reversal.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/a-number-after-a-double-reversal/
+
+class Solution {
+    func createReversedInt(_ num: Int) -> Int {
+        var array = [Int](), temp = num
+        while temp > 0 {
+            array.append(temp % 10)
+            temp /= 10
+        }
+        for int in array {
+            temp = temp * 10 + int
+        }
+        return temp
+    }
+    func isSameAfterReversals(_ num: Int) -> Bool {
+        return createReversedInt(createReversedInt(num)) == num
+    }
+}
