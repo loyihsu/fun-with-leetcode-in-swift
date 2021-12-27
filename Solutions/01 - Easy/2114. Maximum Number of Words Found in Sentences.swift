1f3afceb6b2a506f6cb1f687fb6f19d19f5edd33
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-12-27 21:13:50 +0800

    Create 2114.

---
+++ Solutions/01 - Easy/2114. Maximum Number of Words Found in Sentences.swift
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+class Solution {
+    func mostWordsFound(_ sentences: [String]) -> Int {
+        var max = Int.min
+        for sentence in sentences {
+            let count = sentence.filter({ $0 == " " }).count + 1
+            max = max > count ? max : count
+        }
+        return max
+    }
+}
