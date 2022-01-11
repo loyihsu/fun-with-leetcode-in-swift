af2b2a25685af0d2b13e7d3cd5e870e1a6a768f7
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-11 13:52:07 +0800

    Create 2094.

---
+++ Solutions/01 - Easy/2094. Finding 3-Digit Even Numbers.swift
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/finding-3-digit-even-numbers/
+
+class Solution {
+    func canConstruct(_ number: Int, with digits: [Int: Int]) -> Bool {
+        var temp = [Int: Int](), number = number
+        while number > 0 {
+            temp[number % 10, default: 0] += 1
+            number /= 10
+        }
+        for key in temp.keys where digits[key, default: 0] < temp[key, default: 0] {
+            return false
+        }
+        return true
+    }
+    func findEvenNumbers(_ digits: [Int]) -> [Int] {
+        var output = [Int](), map = [Int: Int]()
+        digits.enumerated().forEach({ map[$1, default: 0] += 1 })
+        for idx in 100...999 where idx % 2 == 0 {
+            if canConstruct(idx, with: map) {
+                output.append(idx)
+            }
+        }
+        return output
+    }
+}
