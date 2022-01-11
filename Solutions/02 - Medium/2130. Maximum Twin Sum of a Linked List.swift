6cf6ad4bc07eb20be06b64d4517bcb8f5ebe86a6
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-11 19:08:52 +0800

    Update 2130.

--- Solutions/02 - Medium/2130. Maximum Twin Sum of a Linked List.swift
+++ Solutions/02 - Medium/2130. Maximum Twin Sum of a Linked List.swift
@@ -2,23 +2,20 @@
 
 class Solution {
     func pairSum(_ head: ListNode?) -> Int {
-        var stack: ListNode?, maximum = Int.min, temp = head, count = 0, idx = 0
-        while let current = temp {
-            count += 1
-            temp = current.next
+        var stack: ListNode?, maximum = Int.min, fast = head, head = head
+        while fast != nil, let current = head {
+            fast = fast?.next?.next
+            head = current.next
+            current.next = stack
+            stack = current
         }
-        temp = head
-        while let current = temp {
-            temp = current.next
-            if idx < count / 2 {
-                current.next = stack
-                stack = current
-            } else if let last = stack {
-                stack = last.next
-                let sum = last.val + current.val
+        while let current = head {
+            if let top = stack {
+                stack = top.next
+                let sum = top.val + current.val
                 maximum = sum > maximum ? sum : maximum
             }
-            idx += 1
+            head = current.next
         }
         return maximum
     }
