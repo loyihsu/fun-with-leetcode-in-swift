cc56db77c49c34a7e7a95c1da3fe804fa1b10b43
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-12 21:38:16 +0800

    Create 2099.

---
+++ Solutions/01 - Easy/2099. Find Subsequence of Length K With the Largest Sum.swift
@@ -0,0 +1,10 @@
+// Problem: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+    func maxSubsequence(_ nums: [Int], _ k: Int) -> [Int] {
+        return nums.enumerated()
+            .sorted(by: { $0.element > $1.element })[0..<k]
+            .sorted(by: { $0.offset < $1.offset })
+            .map({ $0.element })
+    }
+}
