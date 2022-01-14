1608f123b58ce6e34e135ceed41d0be6f7006381
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-14 22:50:53 +0800

    Create 0164.

---
+++ Solutions/03 - Hard/0164. Maximum Gap.swift
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/maximum-gap/
+
+class Solution {
+    func maximumGap(_ nums: [Int]) -> Int {
+        guard nums.count >= 2 else { return 0 }
+        let nums = nums.sorted()
+        var maxGap = 0
+        for idx in 0..<nums.count-1 {
+            maxGap = max(nums[idx+1]-nums[idx], maxGap)
+        }
+        return maxGap
+    }
+}
