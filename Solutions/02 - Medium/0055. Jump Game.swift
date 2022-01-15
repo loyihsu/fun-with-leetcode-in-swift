06e6a958ea8c35b0241a7a1e4286e760da3a4548
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-15 17:27:10 +0800

    Create 0055.

---
+++ Solutions/02 - Medium/0055. Jump Game.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/jump-game/
+
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var table = [Bool](repeating: false, count: nums.count + 1)
+        table[0] = true
+        for idx in nums.indices where nums[idx] != 0 {
+            for jdx in 1...nums[idx] where idx + jdx < table.count {
+                table[idx + jdx] = table[idx]
+            }
+        }
+        return table[nums.count - 1]
+    }
+}
