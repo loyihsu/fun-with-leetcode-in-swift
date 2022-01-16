734b7850ccf86cbd79b6a61efe3dcd28f85f3c36
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-16 21:07:05 +0800

    Create 0119.
    Fix typo.

---
+++ Solutions/01 - Easy/0119. Pascal's Triangle II.swift
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution {
+    func getRow(_ rowIndex: Int) -> [Int] {
+        guard rowIndex > 0 else { return [1] }
+        guard rowIndex > 1 else { return [1, 1] }
+        let prev = getRow(rowIndex - 1)
+        var temp = [Int](repeating: 1, count: rowIndex + 1)
+        for idx in 0..<prev.count - 1 {
+            temp[idx + 1] = prev[idx] + prev[idx + 1]
+        }
+        return temp
+    }
+}
