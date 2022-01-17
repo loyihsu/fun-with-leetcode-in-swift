38b1d06c600173da1094a83e745caa432a0375b2
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-17 21:37:39 +0800

    Create 2138.

---
+++ Solutions/01 - Easy/2138. Divide a String Into Groups of Size k.swift
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/
+
+class Solution {
+    func divideString(_ s: String, _ k: Int, _ fill: Character) -> [String] {
+        let remain = s.count % k
+        var output = [String](repeating: "", count: s.count / k + (remain != 0 ? 1 : 0))
+        var idx = s.startIndex, count = 0
+        while idx < s.endIndex {
+            let notEnding = k <= s.count && idx < s.index(s.endIndex, offsetBy: -k + 1)
+            let last = notEnding ? s.index(idx, offsetBy: k) : s.endIndex
+            output[count] = String(s[idx..<last]) + (notEnding ? "" :  String(repeating: fill, count: k-remain))
+            idx = last
+            count += 1
+        }
+        return output
+    }
+}
