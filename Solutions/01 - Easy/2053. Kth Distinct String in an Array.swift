e12de02a3a612dca9cf8d9d4c5098b04fd9f390b
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-01-18 15:02:36 +0800

    Create 2053.

---
+++ Solutions/01 - Easy/2053. Kth Distinct String in an Array.swift
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+class Solution {
+    func kthDistinct(_ arr: [String], _ k: Int) -> String {
+        var counter = [String: Int](), k = k
+        for item in arr {
+            counter[item, default: 0] += 1
+        }
+        let keys = Set(counter.filter({ $1 == 1 }).keys)
+        for item in arr where keys.contains(item) {
+            k -= 1
+            if k == 0 {
+                return item
+            }
+        }
+        return ""
+    }
+}
