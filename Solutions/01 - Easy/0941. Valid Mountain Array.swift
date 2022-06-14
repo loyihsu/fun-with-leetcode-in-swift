eb906dabe23b7ff3d35e85c387f0230519db4d9e
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-06-14 12:54:23 +0800

    Create 0941.

---
+++ Solutions/01 - Easy/0941. Valid Mountain Array.swift
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/valid-mountain-array/
+
+class Solution {
+    func validMountainArray(_ arr: [Int]) -> Bool {
+        guard arr.count >= 3 else { return false }
+        let maximum = arr.max()
+        var up = true, last: Int? = nil
+
+        for idx in arr.indices {
+            if let last = last {
+                if !(up && arr[idx] > last || !up && arr[idx] < last) {
+                    return false
+                }
+            }
+
+            if arr[idx] == maximum, last != nil, idx != arr.count - 1 {
+                up = false
+            }
+
+            last = arr[idx]
+        }
+
+        return up == false
+    }
+}
