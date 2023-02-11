ce4fd354550f1145e6b2c77cd15347e8c6e2275d
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2023-02-11 16:43:06 +0800

    Create 2558.

---
+++ Solutions/01 - Easy/2558. Take Gifts From the Richest Pile.swift
@@ -0,0 +1,42 @@
+// Problem: https://leetcode.com/problems/take-gifts-from-the-richest-pile/description/
+
+import Foundation
+
+class Solution {
+    func pickGifts(_ gifts: [Int], _ k: Int) -> Int {
+        var gifts = gifts.map(Double.init)
+        return gifts
+            .sqrtMax(times: k)
+            .map(Int.init)
+            .reduce(0, +)
+    }
+}
+
+extension Array where Element == Double {
+    mutating func sqrtMax(times: Int) -> Self {
+        for _ in 0 ..< times {
+            let max = findMax()
+            self = enumerated()
+                .map { index, value in
+                    if index == max {
+                        return sqrt(value)
+                    } else {
+                        return value
+                    }
+                }
+        }
+        return self
+    }
+
+    func findMax() -> Int {
+        var position = 0
+        for idx in indices where max(self[position], self[idx]) != self[position] {
+            position = idx
+        }
+        return position
+    }
+
+    func max(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs >= rhs ? lhs : rhs
+    }
+}
